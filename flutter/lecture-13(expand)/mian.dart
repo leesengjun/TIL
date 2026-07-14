fec0894bfdef11e92a8809bc008d81db602a78eb
leesengjun <s26051@gsm.hs.kr>
2026-07-14 11:04:42 +0900

    Add basic Flutter app structure with layout

--- flutter/lecture-13(expand)/mian.dart
+++ flutter/lecture-13(expand)/mian.dart
@@ -1,1 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomepage(),
+    );
+  }
+}
+
+class MyHomepage extends StatefulWidget {
+  const MyHomepage({super.key});
+
+  @override
+  State<MyHomepage> createState() => _MyHomepageState();
+}
+
+class _MyHomepageState extends State<MyHomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Test Title')),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(height: 150, color: Colors.blue),
+            ),
+            Flexible(
+              flex: 2,
+              child: Container(height: 150, color: Colors.green),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
