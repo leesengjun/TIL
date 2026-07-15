7bddb8ac35b987f6f6f1c7cc5f98c637eab192f3
leesengjun <s26051@gsm.hs.kr>
2026-07-15 18:16:11 +0900

    Update main.dart

--- flutter/lecture-16(다른화면으로 이동)/main.dart
+++ flutter/lecture-16(다른화면으로 이동)/main.dart
@@ -1,1 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:my_first_app/second.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomepage(),
+    );
+  }
+}
+
+class MyHomepage extends StatefulWidget {
+  const MyHomepage({super.key});
+
+  @override
+  State<MyHomepage> createState() => _MyHomepageState();
+}
+
+class _MyHomepageState extends State<MyHomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Test Title')),
+      body: Center(
+        child: GestureDetector(
+          onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => const SecondView()),
+          ),
+          child: Container(
+            padding: const EdgeInsets.all(15),
+            color: Colors.blue,
+            child: const Text('Get stated'),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
